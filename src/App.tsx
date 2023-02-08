1885a45f5c1e488f2edeb37c9c5694695c071135
Deepak Oli <deepak.oli@techprescient.com>
2023-02-08 13:58:21 +0530

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
