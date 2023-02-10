dc241201c999dc0e6cd7f33eb34becda97aa1d6f
Deepak Oli <deepak.oli@techprescient.com>
2023-02-10 15:38:27 +0530

    feat: refactor and add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
-import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
-import SearchBar from "./components/shared/SearchBar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
 
-import { Button } from "react-daisyui";
-import { BsSearch } from "react-icons/bs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col justify-between min-h-screen">
-      <Header />
-      <main className="grow">
-        <div>
-          <form action="">
-            <div className="flex justify-start flex-col p-12 md:flex-row md:px-20 lg:px-28 xl:px-60">
-              {/* search bar */}
-              <SearchBar
-                {...{
-                  placeholder: "Enter Podcast URL",
-                }}
-                className="md:flex-1 md:rounded-r-none"
-              />
-              {/* search button */}
-              <Button
-                className="mt-4 md:mt-0 md:rounded-l-none"
-                type="submit"
-                size="lg"
-                startIcon={<BsSearch color="white" size="1rem" />}
-              >
-                Search
-              </Button>
-            </div>
-          </form>
-        </div>
-      </main>
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Header />
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
